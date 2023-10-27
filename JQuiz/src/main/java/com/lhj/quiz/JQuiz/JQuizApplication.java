beecae7fd45ca9d50a9f3d087eb3181872771b91
vitaminaddiction <leehaejun23@naver.com>
2023-10-27 14:03:02 +0900

    초기화

---
+++ JQuiz/src/main/java/com/lhj/quiz/JQuiz/JQuizApplication.java
@@ -0,0 +1,13 @@
+package com.lhj.quiz.JQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JQuizApplication.class, args);
+	}
+
+}
---
+++ JQuiz/src/test/java/com/lhj/quiz/JQuiz/JQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhj.quiz.JQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
